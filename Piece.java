e2093cb01e8becff1ac29ee534eaec446381f014
blueish4 <michael@hathaways.me.uk>
2014-10-08 10:41:26 +0100

    Initial move checker

--- Piece.java
+++ Piece.java
@@ -25,4 +25,28 @@ public class Piece {
 	public boolean colour() {
 		return this.colour;
 	}
+	//TODO merge these two functions, add in limiter for range
+	public boolean isStraightMove(Coordinate position){
+		//If the move is straight, only one of the coordinates will change
+		int[] currentPos = this.pos.value();
+		int[] nextPos = position.value();
+		if(currentPos[0]==nextPos[0]||currentPos[1]==nextPos[1]){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean isDiagonalMove(Coordinate position){
+		//If the difference between the coordinates is constant, the move is diagonal
+		int[] currentPos = this.pos.value();
+		int[] nextPos = position.value();
+		int currentPosDiff = currentPos[0] - currentPos[1];
+		int nextPosDiff = nextPos[0] - nextPos[1];
+		if(currentPosDiff==nextPosDiff){
+			return true;
+		}else{
+			return false;
+		}
+	}
 }
